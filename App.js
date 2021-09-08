8b5ad1ae7cc557f6964708888b67f309b5439aa6
Acla Putra <muhamadacla22@gmail.com>
2021-09-09 03:11:21 +0700

    modifed app

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+// import 'react-native-gesture-handler'
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>LETS BUILD CHAT appId</Text>
-        <StatusBar style="light" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
     
   );
