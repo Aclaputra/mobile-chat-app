7f276bfdfcb5a1516a554ad4d064eda3fffd9f26
Acla Putra <muhamadacla22@gmail.com>
2021-09-09 01:02:34 +0700

    added firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLPUFb76Gsd_Tg4c_jMMt5sfs-kWqdnX4",
+    authDomain: "signal-clone-dd635.firebaseapp.com",
+    projectId: "signal-clone-dd635",
+    storageBucket: "signal-clone-dd635.appspot.com",
+    messagingSenderId: "894538298111",
+    appId: "1:894538298111:web:d3af4ba567ab539c4212ca"
+  };
