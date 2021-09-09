9c4eabad4e4e5836bdd4aa7770cf6d9cfaba937e
Acla Putra <muhamadacla22@gmail.com>
2021-09-09 19:57:29 +0700

    modified login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Input, Image } from 'react-native-elements'
 import { StatusBar } from "expo-status-bar"
 import codingtopia from "../assets/codingtopia.jpg"
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <View>
             <StatusBar style="light" />
@@ -12,10 +15,12 @@ const LoginScreen = () => {
                 source={codingtopia}
                 style={styles.imageLogo}
             />
-            <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 700, marginTop: 10}}>CODINGTOPIA</Text>
+            {/* <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 700, marginTop: 10}}>CODINGTOPIA</Text> */}
             <View style={styles.inputContainer}>
-                <Input placeholder="Email"/>
-                <Input placeholder="Password" />
+                <Input placeholder="Email" autoFocus type="email" value={email} onChangeText={(text) => setEmail(text)}/>
+                <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={(text) => setPassword(text)}/>
+                <Button containerStyle={styles.button} title="Login" />
+                <Button containerStyle={styles.button} type="outline" title="Register" />
             </View>
         </View> 
     )
@@ -29,5 +34,10 @@ const styles = StyleSheet.create({
         margin: 'auto',
         marginTop: 20,
         borderRadius: 20
-    }
+    },
+    inputContainer: {
+        width: 300,
+        margin: 'auto'
+    },
+    button: {}
 })
