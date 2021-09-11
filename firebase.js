a387ec2dd4c1adb79ac3f249bc9df01a8eb79b80
Acla Putra <muhamadacla22@gmail.com>
2021-09-11 17:37:02 +0700

    modified firebase

--- firebase.js
+++ firebase.js
@@ -8,10 +8,23 @@ import "firebase/firestore";
 //import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLPUFb76Gsd_Tg4c_jMMt5sfs-kWqdnX4",
-    authDomain: "signal-clone-dd635.firebaseapp.com",
-    projectId: "signal-clone-dd635",
-    storageBucket: "signal-clone-dd635.appspot.com",
-    messagingSenderId: "894538298111",
-    appId: "1:894538298111:web:d3af4ba567ab539c4212ca"
-  };
+  apiKey: "AIzaSyCLPUFb76Gsd_Tg4c_jMMt5sfs-kWqdnX4",
+  authDomain: "signal-clone-dd635.firebaseapp.com",
+  projectId: "signal-clone-dd635",
+  storageBucket: "signal-clone-dd635.appspot.com",
+  messagingSenderId: "894538298111",
+  appId: "1:894538298111:web:d3af4ba567ab539c4212ca"
+};
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app()
+}
+
+const db = app.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
