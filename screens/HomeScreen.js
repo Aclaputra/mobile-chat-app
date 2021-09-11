4eee1193e3071d7e31dda1869383dd3528296693
Acla Putra <muhamadacla22@gmail.com>
2021-09-11 17:54:29 +0700

    modified homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 
 const HomeScreen = () => {
     return (
         <View>
+            <StatusBar style="auto" />
             <Text>Homepage</Text>
         </View>
     )
